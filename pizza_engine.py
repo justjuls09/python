bef81ccfeedf723b12c1a059ae889aefb191b01c
justjuls09 <juliehess99@gmail.com>
2026-03-30 20:21:49 -0500

    Update assignment checklist and maintain code structure

--- pizza_engine.py
+++ pizza_engine.py
@@ -2,12 +2,12 @@
 -----------------------------------------------------------------------
 ASSIGNMENT 10A: THE RESILIENT PIZZA ENGINE
 -----------------------------------------------------------------------
-[ ] 1. Header Docstring included.
-[ ] 2. Global constant TOPPINGS defined as a Tuple in ALL_CAPS.
-[ ] 3. Function 'make_pizza' defines 4 specific parameters.
-[ ] 4. 'make_pizza' uses a DEFAULT value for is_delivery.
-[ ] 5. main() displays the Global Pantry list to the user.
-[ ] 6. main() calls the function using KEYWORD ARGUMENTS.
+[x] 1. Header Docstring included.
+[x] 2. Global constant TOPPINGS defined as a Tuple in ALL_CAPS.
+[x] 3. Function 'make_pizza' defines 4 specific parameters.
+[x] 4. 'make_pizza' uses a DEFAULT value for is_delivery.
+[x] 5. main() displays the Global Pantry list to the user.
+[x] 6. main() calls the function using KEYWORD ARGUMENTS.
 -----------------------------------------------------------------------
 """
 
